6b52e2948d354ac6e48c3938e56e5364dd568f47
Hoang5433 <hoangquy3001@gmail.com>
2025-03-08 08:49:00 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/GUI/MainFrame.java

---
+++ src/main/java/GUI/Panel/EmployeePanel.java
@@ -0,0 +1,13 @@
+package GUI.Panel;
+
+import javax.swing.*;
+
+public class EmployeePanel extends JPanel {
+    public EmployeePanel() {
+        initComponent();
+    }
+    private void initComponent() {
+        setLayout(null);
+        setBounds(100, 100, 450, 300);
+    }
+}
