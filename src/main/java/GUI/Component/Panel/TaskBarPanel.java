5c613add7be70ebdbc0369d21bc8a9fdfe044abd
Hoang5433 <hoangquy3001@gmail.com>
2025-04-05 17:47:11 +0700

    add currentUser

--- src/main/java/GUI/Component/Panel/TaskBarPanel.java
+++ src/main/java/GUI/Component/Panel/TaskBarPanel.java
@@ -1,5 +1,6 @@
 package GUI.Component.Panel;
 
+import DTO.Employee;
 import GUI.Component.Panel.Components.AccountNameLabel;
 import GUI.Component.Panel.Components.ItemSideBarLabel;
 import GUI.Component.Panel.Components.SidebarListener;
@@ -11,13 +12,26 @@ public class TaskBarPanel extends JPanel {
     ItemSideBarLabel itemSideBarLabel;
     AccountNameLabel accountNameLabel;
     JFrame parentFrame;
-    public TaskBarPanel(JFrame parentFrame, SidebarListener listener) {
+    public TaskBarPanel(JFrame parentFrame, SidebarListener listener, Employee currentEmployee) {
+        String fullName = currentEmployee.getFirstName() + " " + currentEmployee.getLastName();
+        Long roleId = currentEmployee.getRoleID();
+        String roleName = "";
+        if (roleId == 1L) {
+            roleName = "Admin";
+        } else if (roleId == 2L) {
+            roleName = "Quản lý";
+        } else if (roleId == 3L) {
+            roleName = "Nhân viên";
+        }
         this.parentFrame = parentFrame;
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.setPreferredSize(new Dimension(252, parentFrame.getHeight()));
-        accountNameLabel = new AccountNameLabel("Hoàng Đình Phú Quý", "Admin");
+        accountNameLabel = new AccountNameLabel(fullName, roleName);
         add(accountNameLabel);
         itemSideBarLabel = new ItemSideBarLabel(parentFrame, listener);
         this.add(itemSideBarLabel);
+    }
+    public void setSelectedItem(String itemName) {
+        itemSideBarLabel.setSelectedItem(itemName);
     }
 }
